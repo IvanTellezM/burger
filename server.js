892e8076f7e19b6c1e74e186682184c13d63b09a
Ivan Tellez <ivantellezm@gmail.com>
2018-06-07 14:56:54 -0700

    started app setup

---
+++ server.js
@@ -0,0 +1,14 @@
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodoverride = require(method - override);
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
